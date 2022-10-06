1aecf420dcf3c640d53134b1f301266c9cd78fa5
Vinnet Rathi <vineet.rathi@tothenew.com>
2022-10-06 17:05:42 +0530

    initial commit

---
+++ src/Ans5.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Ans5 {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public static void main(String[] args) {
+        Ans5 Example = new Ans5();
+        new Thread(() -> {
+            Example.lock.lock();
+            System.out.println("Lock Thread 1");
+            try {
+                Thread.sleep(5000);
+                Example.condition.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread running");
+            Example.lock.unlock();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Example.lock.lock();
+            System.out.println("Lock Thread 2");
+            Example.condition.signal();
+            System.out.println("Thread finished working");
+            Example.lock.unlock();
+        }).start();
+    }
+}
